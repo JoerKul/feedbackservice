9d130045a5774e339a1e25ff8da12d1daada6a3c
JoerKul <osvald@gmx.de>
2021-11-14 20:07:04 +0100

    feat: :sparkles: add stats functions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,10 +18,16 @@
 			rating: 8
 		}
 	];
+
+	$: count = feedbacks.length;
+	let avg = 'to be defined';
 </script>
 
 <div class="container mx-auto px-4 ">
-	<h1>Welcome to SvelteKit</h1>
-
+	<h1 class="text-5xl">Welcome</h1>
+	<div class="flex justify-between text-2xl mt-6 mb-16">
+		<div>{count} Reviews</div>
+		<div>Average Rating: {avg}</div>
+	</div>
 	<FeedbackList {feedbacks} />
 </div>
